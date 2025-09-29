3c59a4e6e99dd5bd8d3a548c00c12cf4f51e6a1e
trevornei <117675657+trevornei@users.noreply.github.com>
2025-09-29 09:45:53 -0600

    Made a comment explaining debugging thoughts...

--- browser_identity/src/main.js
+++ browser_identity/src/main.js
@@ -4,6 +4,9 @@ let geo = navigator.geolocation;
 let geoPos = Geolocation.getCurrentPostition;
 let geoDelta = Geolocation.getCurrentPostition;
 
+// This was not working...
+// I believe that this is because the function needs to be asynchronous? 
+// Eh, I want to print this to the DOM and not the console anyways!
 console.log(`The value of your geo location {OBJECT} is: ${geo}`)
 
 console.log(`The value of your geo location is: ${geoPos}`)
