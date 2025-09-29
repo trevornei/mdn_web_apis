55dafe3b9060a406f7a5f869f02759b755ef3936
trevornei <117675657+trevornei@users.noreply.github.com>
2025-09-29 09:51:48 -0600

    Updated learning from research. Navigator.geolocation{obj}

--- browser_identity/src/main.js
+++ browser_identity/src/main.js
@@ -1,17 +1,13 @@
 import './style.css'
 
+// Findings: 
+// 1. Navigator.geolocation allows access to methods NOT objects...
 let geo = navigator.geolocation;
+// So, I assuming that the pseudo code would look like:
+// async function <geo_var>() { return .methods(available) }
 let geoPos = Geolocation.getCurrentPostition;
 let geoDelta = Geolocation.getCurrentPostition;
 
-// This was not working...
-// I believe that this is because the function needs to be asynchronous? 
-// Eh, I want to print this to the DOM and not the console anyways!
-console.log(`The value of your geo location {OBJECT} is: ${geo}`)
-
-console.log(`The value of your geo location is: ${geoPos}`)
-console.log(`The changed value of your geo location is: ${geoDelta}`)
-console.log(`The value of your geo location is: ${geo}`)
 
 document.querySelector('#app').innerHTML = `
   <div allow="geolocation">
